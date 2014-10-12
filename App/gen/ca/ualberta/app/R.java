af15fbdbf7699a181a20bad6dcb8ef192a006874
xiaocong <xiaocong@ualberta.ca>
2014-10-11 18:42:29 -0600

    I upload some layouts

--- App/gen/ca/ualberta/app/R.java
+++ App/gen/ca/ualberta/app/R.java
@@ -26,22 +26,60 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int arrowup=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int login1=0x7f020002;
+        public static final int login2=0x7f020003;
+        public static final int profile=0x7f020004;
+        public static final int question=0x7f020005;
+        public static final int question1=0x7f020006;
+        public static final int setting=0x7f020007;
+        public static final int user=0x7f020008;
+        public static final int user1=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int AQuestionTextView=0x7f080000;
+        public static final int MyQuestionTextView=0x7f080015;
+        public static final int action_settings=0x7f080019;
+        public static final int answerEditText1=0x7f080002;
+        public static final int answerOfMyquestionListView=0x7f080008;
+        public static final int askQuestionButton=0x7f080012;
+        public static final int authorNameTextView=0x7f080006;
+        public static final int cancelAnswerButton=0x7f080003;
+        public static final int current1Question=0x7f080011;
+        public static final int currentAnswertextView=0x7f080001;
+        public static final int currentQuestionButton=0x7f080009;
+        public static final int editMyquestionButton=0x7f080017;
+        public static final int listView1=0x7f080014;
+        public static final int localQuestionButton=0x7f08000b;
+        public static final int longinButton=0x7f08000d;
+        public static final int myQuestiontextView=0x7f080007;
+        public static final int profielPicImageView=0x7f080005;
+        public static final int profileButton=0x7f08000a;
+        public static final int questionTextView=0x7f080018;
+        public static final int removeMyquestionButton=0x7f080016;
+        public static final int settingButton=0x7f08000c;
+        public static final int sortButton=0x7f080013;
+        public static final int submitAnswerbutton=0x7f080004;
+        public static final int usernameButton=0x7f08000f;
+        public static final int usernameEditText=0x7f08000e;
+        public static final int usernametextView=0x7f080010;
     }
     public static final class layout {
         public static final int activity_answer=0x7f030000;
         public static final int activity_author_profile=0x7f030001;
-        public static final int activity_login=0x7f030002;
-        public static final int activity_question_list=0x7f030003;
+        public static final int activity_dashboard=0x7f030002;
+        public static final int activity_login=0x7f030003;
+        public static final int activity_question_list=0x7f030004;
+        public static final int one_of_myquestion=0x7f030005;
+        public static final int single_question=0x7f030006;
     }
     public static final class menu {
         public static final int answer=0x7f070000;
         public static final int author_profile=0x7f070001;
-        public static final int login=0x7f070002;
-        public static final int question_list=0x7f070003;
+        public static final int dashboard=0x7f070002;
+        public static final int login=0x7f070003;
+        public static final int question_list=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -49,6 +87,7 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int title_activity_answer=0x7f050004;
         public static final int title_activity_author_profile=0x7f050005;
+        public static final int title_activity_dashboard=0x7f050006;
         public static final int title_activity_question_list=0x7f050003;
     }
     public static final class style {
