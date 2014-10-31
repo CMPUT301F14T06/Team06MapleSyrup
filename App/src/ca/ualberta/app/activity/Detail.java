f89dc4d458d4d642ea7cb3ff0afc0481ba137832
liwenCHEN20 <liwen1@ualberta.ca>
2014-10-31 16:31:01 -0600

    i add a detail and a my_question xml and a Detail activity in

---
+++ App/src/ca/ualberta/app/activity/Detail.java
@@ -0,0 +1,36 @@
+package ca.ualberta.app.activity;
+
+
+import java.util.ArrayList;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+
+
+public class Detail extends Activity
+{
+	
+	
+	private View detail;
+	private ListView listv;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.detail);
+		
+		
+		}
+	
+	
+	
+	
+	
+	
+}
