de487b9fbc232b3a5197e01176fcf3da0245a6b5
bichengyan <boyer8806@hotmail.com>
2014-11-04 14:49:59 -0700

    fixed spinner color

--- App/src/ca/ualberta/app/activity/FragmentSearch.java
+++ App/src/ca/ualberta/app/activity/FragmentSearch.java
@@ -4,20 +4,33 @@ import ca.ualberta.app.activity.R;
 import ca.ualberta.app.adapter.QuestionListAdapter;
 import ca.ualberta.app.controller.QuestionListController;
 import ca.ualberta.app.models.QuestionListManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 //The fragment part is from this website: http://www.programering.com/a/MjNzIDMwATI.html 2014-Oct-20
 public class FragmentSearch extends Fragment {
+	static String sortByDate = "Sort By Date";
+	static String sortByScore = "Sort By Score";
+	static String sortByQuestionUpvote = "Sort By Question Upvote";
+	static String sortByAnswerUpvote = "Sort By Answer Upvote";
+	static String sortByPicture = "Sort By Picture";
+	static String[] sortOption = { sortByDate, sortByScore, sortByPicture,
+			sortByQuestionUpvote, sortByAnswerUpvote };
+
 	private TextView titleBar;
 	private EditText searchEditText;
 	private QuestionListController questionListController;
@@ -25,6 +38,11 @@ public class FragmentSearch extends Fragment {
 	private QuestionListAdapter adapter = null;
 	private ListView searchResultListView;
 	private Button searchButton;
+	private Spinner sortOptionSpinner;
+	private Context mcontext;
+	private ArrayAdapter<?> spin_adapter;
+	private static long categoryID;
+	private String sortString = null;
 	private int haveSearchResult = 0;
 
 	// Thread to update adapter after an operation
@@ -35,12 +53,14 @@ public class FragmentSearch extends Fragment {
 						"No matched results", Toast.LENGTH_LONG).show();
 			}
 			adapter.notifyDataSetChanged();
+			spin_adapter.notifyDataSetChanged();
 		}
 	};
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		mcontext = getActivity().getApplicationContext();
 		return inflater.inflate(R.layout.fragment_search, container, false);
 	}
 
@@ -55,6 +75,8 @@ public class FragmentSearch extends Fragment {
 				R.id.searchQuestion_ListView);
 		searchButton = (Button) getView().findViewById(
 				R.id.searchQuestion_Button);
+		sortOptionSpinner = (Spinner) getView().findViewById(
+				R.id.search_sort_spinner);
 
 	}
 
@@ -68,19 +90,68 @@ public class FragmentSearch extends Fragment {
 				questionListController.getQuestionArrayList());
 
 		searchResultListView.setAdapter(adapter);
+		spin_adapter = new ArrayAdapter<String>(mcontext,
+				R.layout.spinner_item, sortOption);
 
+		sortOptionSpinner.setAdapter(spin_adapter);
+		sortOptionSpinner
+				.setOnItemSelectedListener(new change_category_click());
 		searchButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				questionListController.clear();
-				String searchString = searchEditText.getText().toString();
-				searchEditText.setText("");
-				Thread thread = new SearchThread(searchString);
-				thread.start();
+				updateSearchList();
 			}
 		});
+	}
+
+	private class change_category_click implements OnItemSelectedListener {
+		public void onItemSelected(AdapterView<?> parent, View view,
+				int position, long id) {
+			categoryID = position;
+
+			// sort by Date
+			if (categoryID == 0) {
+				sortString = "date";
+				updateSearchList();
+			}
+
+			// sort by Picture
+			if (categoryID == 1) {
+				sortString = "picture";
+				updateSearchList();
+			}
+
+			// sort by Score
+			if (categoryID == 2) {
+				sortString = "score";
+				updateSearchList();
+			}
+			// sort by Question upvote
+			if (categoryID == 3) {
+				sortString = "q_upvote";
+				updateSearchList();
+			}
+
+			// sort by Answer upvote
+			if (categoryID == 4) {
+				sortString = "a_upvote";
+				updateSearchList();
+			}
+		}
+
+		public void onNothingSelected(AdapterView<?> parent) {
+			sortOptionSpinner.setSelection(0);
+		}
+	}
+
+	private void updateSearchList() {
+		questionListController.clear();
+		String searchString = searchEditText.getText().toString();
+		searchEditText.setText("");
+		Thread thread = new SearchThread(searchString);
+		thread.start();
 	}
 
 	class SearchThread extends Thread {
@@ -95,8 +166,9 @@ public class FragmentSearch extends Fragment {
 		public void run() {
 			questionListController.clear();
 			questionListController.addAll(questionListManager.searchQuestions(
-					search, null, null));
-			if (questionListManager.searchQuestions(search, null,null).size() != 0) {
+					search, null, sortString));
+			if (questionListManager.searchQuestions(search, null, sortString)
+					.size() != 0) {
 				haveSearchResult = 1;
 			} else {
 				haveSearchResult = 0;
