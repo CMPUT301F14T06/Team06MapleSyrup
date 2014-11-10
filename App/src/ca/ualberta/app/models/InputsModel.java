ef355b17b981b25390d312f8f001ef88ec367ee2
xiaocong <xiaocong@ualberta.ca>
2014-11-10 03:23:48 -0700

    jdoc

--- App/src/ca/ualberta/app/models/InputsModel.java
+++ App/src/ca/ualberta/app/models/InputsModel.java
@@ -24,6 +24,12 @@ import java.util.Date;
 
 import android.graphics.Bitmap;
 
+/**
+ * This class will handle all kinds of user input
+ * 
+ * @author Anni
+ *
+ */
 public abstract class InputsModel {
 	public String title;
 	public String content;
@@ -31,7 +37,14 @@ public abstract class InputsModel {
 	Bitmap image = null;
 	Date timestamp;
 	
-	// Question contains replyList and answerList
+	/**
+	 * Question contains replyList and answerList
+	 * 
+	 * @param content The content.
+	 * @param userName The ID of the user.
+	 * @param title The title of the question.
+	 * @param image The image in the question.
+	 */
 	public InputsModel(String content, String userName, String title,  
 			Bitmap image) {
 		this.timestamp = new Date();
@@ -41,7 +54,13 @@ public abstract class InputsModel {
 		this.userName = userName;
 	}
 
-	// Answer contain replyList
+	/**
+	 * Answer contain replyList
+	 * 
+	 * @param content The content.
+	 * @param userName The ID of the user.
+	 * @param image The image in the question.
+	 */
 	public InputsModel(String content, String userName, Bitmap image) {
 		timestamp = new Date();
 		this.image = image;
@@ -49,41 +68,86 @@ public abstract class InputsModel {
 		this.userName = userName;
 	}
 
-	// Reply
+	/**
+	 * The reply
+	 * 
+	 * @param content The content.
+	 * @param userName The ID of the user.
+	 */
 	public InputsModel(String content, String userName) {
 		timestamp = new Date();
 		this.content = content;
 		this.userName = userName;
 	}
 
+	/**
+	 * Check if the question/answer has an image
+	 * 
+	 * @return true if the question/answer has an image, otherwise, return false.
+	 */
 	public boolean hasImage() {
 		return image != null;
 	}
 
+	/**
+	 * The image of the question/answer
+	 * 
+	 * @param image The image of the question/answer.
+	 */
 	public void setImage(Bitmap image) {
 		this.image = image;
 	}
 
+	/**
+	 * Return the image
+	 * 
+	 * @return the image.
+	 */
 	public Bitmap getImage() {
 		return image;
 	}
 
+	/**
+	 * Edit the content of a question/answer
+	 * 
+	 * @param newContent The new content.
+	 */
 	public void editContent(String newContent) {
 		this.content = newContent;
 	}
 
+	/**
+	 * Return the content of a question/answer
+	 * 
+	 * @return The content of a question/answer.
+	 */
 	public String getContent() {
 		return content;
 	}
 
+	/**
+	 * Return date and time
+	 * 
+	 * @return timestamp, date and time.
+	 */
 	public Date getTimestamp() {
 		return timestamp;
 	}
 
+	/**
+	 * Set date and time
+	 * 
+	 * @param timestamp the new date and time.
+	 */
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
 	
+	/**
+	 * Return the user name of an author
+	 * 
+	 * @return userName The user name of the author.
+	 */
 	public String getAuthor() {
 		return userName;
 	}
