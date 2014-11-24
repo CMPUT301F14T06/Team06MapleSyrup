4486d403c20c827270157cf41ad45f543cc6ab23
Annieday <adai1@ualberta.ca>
2014-11-23 21:20:38 -0700

    add gitignore file

--- App/gen/ca/ualberta/app/activity/R.java
+++ App/gen/ca/ualberta/app/activity/R.java
@@ -74,65 +74,74 @@ public final class R {
         public static final int picture_icon48=0x7f020026;
         public static final int profile=0x7f020027;
         public static final int profile_btn_bg=0x7f020028;
-        public static final int question=0x7f020029;
-        public static final int question1=0x7f02002a;
-        public static final int round_plus_icon24=0x7f02002b;
-        public static final int round_plus_icon48=0x7f02002c;
-        public static final int save_button_style=0x7f02002d;
-        public static final int save_icon24=0x7f02002e;
-        public static final int save_icon32=0x7f02002f;
-        public static final int save_icon48=0x7f020030;
-        public static final int search_icon=0x7f020031;
-        public static final int separator=0x7f020032;
-        public static final int setting=0x7f020033;
-        public static final int sq_plus_icon32=0x7f020034;
-        public static final int star_fav_empty_icon24=0x7f020035;
-        public static final int star_fav_empty_icon32=0x7f020036;
-        public static final int star_fav_empty_icon48=0x7f020037;
-        public static final int star_fav_icon24=0x7f020038;
-        public static final int star_fav_icon32=0x7f020039;
-        public static final int star_fav_icon48=0x7f02003a;
-        public static final int title_bar=0x7f02003b;
-        public static final int toolbar_bg=0x7f02003c;
-        public static final int trash_icon16=0x7f02003d;
-        public static final int trash_icon24=0x7f02003e;
-        public static final int trash_icon32=0x7f02003f;
-        public static final int trash_icon48=0x7f020040;
-        public static final int user=0x7f020041;
-        public static final int user1=0x7f020042;
-        public static final int user_icon=0x7f020043;
-        public static final int user_icon48=0x7f020044;
-        public static final int user_photo=0x7f020045;
-        public static final int wrench_plus_2_icon32=0x7f020046;
-        public static final int wrench_plus_2_icon48=0x7f020047;
-        public static final int xlistview_arrow=0x7f020048;
+        public static final int progress_1=0x7f020029;
+        public static final int progress_2=0x7f02002a;
+        public static final int progress_3=0x7f02002b;
+        public static final int progress_4=0x7f02002c;
+        public static final int progress_5=0x7f02002d;
+        public static final int progress_6=0x7f02002e;
+        public static final int progress_7=0x7f02002f;
+        public static final int progress_8=0x7f020030;
+        public static final int progress_round=0x7f020031;
+        public static final int question=0x7f020032;
+        public static final int question1=0x7f020033;
+        public static final int round_plus_icon24=0x7f020034;
+        public static final int round_plus_icon48=0x7f020035;
+        public static final int save_button_style=0x7f020036;
+        public static final int save_icon24=0x7f020037;
+        public static final int save_icon32=0x7f020038;
+        public static final int save_icon48=0x7f020039;
+        public static final int search_icon=0x7f02003a;
+        public static final int separator=0x7f02003b;
+        public static final int setting=0x7f02003c;
+        public static final int sq_plus_icon32=0x7f02003d;
+        public static final int star_fav_empty_icon24=0x7f02003e;
+        public static final int star_fav_empty_icon32=0x7f02003f;
+        public static final int star_fav_empty_icon48=0x7f020040;
+        public static final int star_fav_icon24=0x7f020041;
+        public static final int star_fav_icon32=0x7f020042;
+        public static final int star_fav_icon48=0x7f020043;
+        public static final int title_bar=0x7f020044;
+        public static final int toolbar_bg=0x7f020045;
+        public static final int trash_icon16=0x7f020046;
+        public static final int trash_icon24=0x7f020047;
+        public static final int trash_icon32=0x7f020048;
+        public static final int trash_icon48=0x7f020049;
+        public static final int user=0x7f02004a;
+        public static final int user1=0x7f02004b;
+        public static final int user_icon=0x7f02004c;
+        public static final int user_icon48=0x7f02004d;
+        public static final int user_photo=0x7f02004e;
+        public static final int wrench_plus_2_icon32=0x7f02004f;
+        public static final int wrench_plus_2_icon48=0x7f020050;
+        public static final int xlistview_arrow=0x7f020051;
     }
     public static final class id {
-        public static final int ImageView01=0x7f0a0066;
-        public static final int ImageView02=0x7f0a0067;
-        public static final int Latitude=0x7f0a0074;
-        public static final int Longitute=0x7f0a0077;
-        public static final int TextView01=0x7f0a0073;
-        public static final int TextView03=0x7f0a0076;
-        public static final int action_settings=0x7f0a0079;
+        public static final int ImageView01=0x7f0a0068;
+        public static final int ImageView02=0x7f0a0069;
+        public static final int Latitude=0x7f0a0076;
+        public static final int Longitute=0x7f0a0079;
+        public static final int TextView01=0x7f0a0075;
+        public static final int TextView03=0x7f0a0078;
+        public static final int action_settings=0x7f0a007b;
         public static final int add_answer_pic=0x7f0a0007;
         public static final int add_menu_button=0x7f0a0022;
         public static final int add_question_pic=0x7f0a0015;
-        public static final int answerContent=0x7f0a005e;
-        public static final int answerContentTextView=0x7f0a005f;
-        public static final int answerImage=0x7f0a0060;
+        public static final int answerContent=0x7f0a0060;
+        public static final int answerContentTextView=0x7f0a0061;
+        public static final int answerImage=0x7f0a0062;
         public static final int answerStateTextView=0x7f0a0035;
         public static final int answer_content_editText=0x7f0a0002;
         public static final int answer_content_textView=0x7f0a0001;
         public static final int answer_image_imageView=0x7f0a0003;
-        public static final int answer_layout=0x7f0a005d;
+        public static final int answer_layout=0x7f0a005f;
         public static final int answer_listView=0x7f0a0041;
-        public static final int answer_reply_button=0x7f0a0062;
+        public static final int answer_reply_button=0x7f0a0064;
         public static final int answer_reply_cancel_button=0x7f0a000b;
         public static final int answer_reply_editText=0x7f0a0009;
         public static final int answer_reply_submit_button=0x7f0a000a;
         public static final int answer_reply_textView=0x7f0a0008;
-        public static final int answer_time_textView=0x7f0a0061;
+        public static final int answer_time_textView=0x7f0a0063;
         public static final int authorInformation=0x7f0a002c;
         public static final int authorNameTextView=0x7f0a002f;
         public static final int authorPic=0x7f0a002e;
@@ -142,21 +151,23 @@ public final class R {
         public static final int changePhotoButton=0x7f0a0049;
         public static final int create_answer_menu=0x7f0a0000;
         public static final int create_question_menu=0x7f0a000c;
-        public static final int expandIndicator=0x7f0a0065;
+        public static final int expandIndicator=0x7f0a0067;
         public static final int favQuestion_ListView=0x7f0a0026;
         public static final int favSort_spinner=0x7f0a0024;
-        public static final int fav_button=0x7f0a006d;
+        public static final int fav_button=0x7f0a006f;
         public static final int fav_detail_button=0x7f0a0037;
         public static final int fav_question=0x7f0a004d;
         public static final int fragement_main=0x7f0a001e;
         public static final int fragement_profile=0x7f0a001f;
         public static final int gridLayout1=0x7f0a0033;
-        public static final int imageView1=0x7f0a0063;
+        public static final int id_tv_loadingmsg=0x7f0a0054;
+        public static final int imageView1=0x7f0a0065;
         public static final int large_image=0x7f0a0044;
-        public static final int linearLayout1=0x7f0a0068;
-        public static final int linearLayout2=0x7f0a0075;
+        public static final int linearLayout1=0x7f0a006a;
+        public static final int linearLayout2=0x7f0a0077;
         public static final int linearLayout3=0x7f0a002d;
         public static final int linearLayout4=0x7f0a003a;
+        public static final int loadingImageView=0x7f0a0053;
         public static final int localQuestion_ListView=0x7f0a0028;
         public static final int localSort_spinner=0x7f0a0027;
         public static final int local_cache=0x7f0a004c;
@@ -174,14 +185,14 @@ public final class R {
         public static final int one_title=0x7f0a0025;
         public static final int profile_menu=0x7f0a004b;
         public static final int profile_menu_button=0x7f0a0023;
-        public static final int questionContentTextView=0x7f0a006a;
+        public static final int questionContentTextView=0x7f0a006c;
         public static final int questionDescription=0x7f0a0030;
         public static final int questionDetailContentTextView=0x7f0a0032;
         public static final int questionDetailTitleTextView=0x7f0a0031;
         public static final int questionImage=0x7f0a0039;
-        public static final int questionPic=0x7f0a006b;
+        public static final int questionPic=0x7f0a006d;
         public static final int questionTimeTextView=0x7f0a003b;
-        public static final int questionTitleTextView=0x7f0a0069;
+        public static final int questionTitleTextView=0x7f0a006b;
         public static final int question_Button=0x7f0a0047;
         public static final int question_EditText=0x7f0a0046;
         public static final int question_ListView=0x7f0a0048;
@@ -196,21 +207,21 @@ public final class R {
         public static final int question_reply_expanableListView=0x7f0a003f;
         public static final int question_title_editText=0x7f0a000e;
         public static final int question_title_textView=0x7f0a000d;
-        public static final int replyAuthor_textView=0x7f0a0071;
-        public static final int reply_group_expandIndicator=0x7f0a0072;
-        public static final int reply_textView=0x7f0a006f;
-        public static final int reply_time_textView=0x7f0a0070;
-        public static final int save_button=0x7f0a006c;
+        public static final int replyAuthor_textView=0x7f0a0073;
+        public static final int reply_group_expandIndicator=0x7f0a0074;
+        public static final int reply_textView=0x7f0a0071;
+        public static final int reply_time_textView=0x7f0a0072;
+        public static final int save_button=0x7f0a006e;
         public static final int save_detail_button=0x7f0a0036;
-        public static final int scrolllistview_footer_content=0x7f0a0053;
-        public static final int scrolllistview_footer_hint_textview=0x7f0a0055;
-        public static final int scrolllistview_footer_progressbar=0x7f0a0054;
-        public static final int scrolllistview_header_arrow=0x7f0a005a;
-        public static final int scrolllistview_header_content=0x7f0a0056;
-        public static final int scrolllistview_header_hint_textview=0x7f0a0058;
-        public static final int scrolllistview_header_progressbar=0x7f0a005b;
-        public static final int scrolllistview_header_text=0x7f0a0057;
-        public static final int scrolllistview_header_time=0x7f0a0059;
+        public static final int scrolllistview_footer_content=0x7f0a0055;
+        public static final int scrolllistview_footer_hint_textview=0x7f0a0057;
+        public static final int scrolllistview_footer_progressbar=0x7f0a0056;
+        public static final int scrolllistview_header_arrow=0x7f0a005c;
+        public static final int scrolllistview_header_content=0x7f0a0058;
+        public static final int scrolllistview_header_hint_textview=0x7f0a005a;
+        public static final int scrolllistview_header_progressbar=0x7f0a005d;
+        public static final int scrolllistview_header_text=0x7f0a0059;
+        public static final int scrolllistview_header_time=0x7f0a005b;
         public static final int separator1=0x7f0a003e;
         public static final int separator2=0x7f0a0040;
         public static final int setAuthorName=0x7f0a004a;
@@ -220,10 +231,10 @@ public final class R {
         public static final int submit_reply_button=0x7f0a0018;
         public static final int take_answer_pic=0x7f0a0006;
         public static final int take_question_pic=0x7f0a0014;
-        public static final int textView1=0x7f0a0064;
-        public static final int textView2=0x7f0a005c;
-        public static final int timeTextView=0x7f0a006e;
-        public static final int titleTv=0x7f0a0078;
+        public static final int textView1=0x7f0a0066;
+        public static final int textView2=0x7f0a005e;
+        public static final int timeTextView=0x7f0a0070;
+        public static final int titleTv=0x7f0a007a;
         public static final int upvoteStateTextView=0x7f0a0034;
         public static final int upvote_button=0x7f0a0038;
         public static final int username_editText=0x7f0a001b;
@@ -246,15 +257,16 @@ public final class R {
         public static final int dialog_photo=0x7f03000b;
         public static final int fragment_main=0x7f03000c;
         public static final int fragment_profile=0x7f03000d;
-        public static final int scrolllistview_footer=0x7f03000e;
-        public static final int scrolllistview_header=0x7f03000f;
-        public static final int single_answer=0x7f030010;
-        public static final int single_question=0x7f030011;
-        public static final int single_reply=0x7f030012;
-        public static final int single_reply_group=0x7f030013;
-        public static final int spinner_item=0x7f030014;
-        public static final int testgps=0x7f030015;
-        public static final int title_bar=0x7f030016;
+        public static final int progress_dialog=0x7f03000e;
+        public static final int scrolllistview_footer=0x7f03000f;
+        public static final int scrolllistview_header=0x7f030010;
+        public static final int single_answer=0x7f030011;
+        public static final int single_question=0x7f030012;
+        public static final int single_reply=0x7f030013;
+        public static final int single_reply_group=0x7f030014;
+        public static final int spinner_item=0x7f030015;
+        public static final int testgps=0x7f030016;
+        public static final int title_bar=0x7f030017;
     }
     public static final class menu {
         public static final int answer=0x7f090000;
@@ -345,12 +357,14 @@ public final class R {
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
 
 
-
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
+
+
 
         
 
@@ -370,6 +384,10 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f080001;
+        /**  Following code is from http://blog.csdn.net/rohsuton/article/details/7518031 
+         */
+        public static final int CustomDialog=0x7f080007;
+        public static final int CustomProgressDialog=0x7f080008;
         public static final int answer_reply_button=0x7f080005;
         public static final int create_menu_bottom=0x7f080003;
         public static final int fav_save_upvote_button=0x7f080004;
