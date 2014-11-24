7bbbac825d0460fe8432df98ec68c5156d06d299
jingjing3 <jliang3@ualberta.ca>
2014-11-24 13:29:01 -0700

    JUnitTest

--- AppTest/src/ca/ualberta/app/test/ModelTest/AuthorTest.java
+++ AppTest/src/ca/ualberta/app/test/ModelTest/AuthorTest.java
@@ -1,5 +1,8 @@
 package ca.ualberta.app.test.ModelTest;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import junit.framework.TestCase;
 import ca.ualberta.app.models.Author;
 
@@ -12,7 +15,23 @@ public class AuthorTest extends TestCase {
 		assertTrue("Author Login Name is not equal",
 				username.equals(author.getUsername()));
 	}
-	public void TestSetUsername(){
+	
+	public void testAddAquestion(){
+        String username = "username";
+	Author author = new Author(username);
+       
+        Long questionID1=new Date().getTime();
+        Long questionID2=new Date().getTime();
+
+        ArrayList<Long> myQuestionID= new ArrayList<Long>();
+
+        author.addAQuestion(questionID1);
+        author.addAQuestion(questionID2);
+        assertEquals(questionID1,author.getAuthorQuestionId().get(0));
+        assertEquals(author.getAuthorQuestionId().size(),2);
+   }
+	
+	public void testSetUsername(){
         String username = "username";
         String newUsername = "Robin";
 	    Author author = new Author(username); 
---
+++ AppTest/src/ca/ualberta/app/test/ModelTest/QuestionAddTest.java
@@ -0,0 +1,175 @@
+package ca.ualberta.app.test.ModelTest;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import ca.ualberta.app.models.Answer;
+import ca.ualberta.app.models.Author;
+import ca.ualberta.app.models.Question;
+import ca.ualberta.app.models.Reply;
+import junit.framework.TestCase;
+
+public class QuestionAddTest extends TestCase {
+	//test initial Question object
+		public void testQuestion() {
+			String username = "username";
+			String questionString = "A question";
+			String titleString = "A title";
+			String imageByteArray = null;
+			Author author = new Author( username);
+			Question question = new Question(questionString, author.getUsername(),
+					titleString, imageByteArray);
+			assertEquals(questionString, question.getContent());
+		}
+
+		public void testAddAnswer() {
+			//create a Question and an Answer object
+			String username = "username";
+			String questionString = "A qustion";
+			String answerString = "A answer";
+			String titleString = "A title";
+			String imageByteArray = null;
+			Author author = new Author(username);
+			Answer answer = new Answer(answerString, author.getUsername(), imageByteArray);
+			Question question = new Question(questionString, author.getUsername(),
+					titleString, imageByteArray);
+			
+			//add the answer to Question
+			question.addAnswer(answer);
+			
+			//check the result
+			assertTrue("Answer is not equal",
+					answerString.equals(answer.getContent()));
+		}
+
+		public void testAddReply() {
+			//create a Question and a Reply Object
+			String username = "username";
+			String questionString = "A qustion";
+			String replyString = "A reply";
+			String titleString = "A title";
+			String imageByteArray = null;
+			Author author = new Author(username);
+			Reply reply = new Reply(replyString, author.getUsername());
+			Question question = new Question(questionString, author.getUsername(),
+					titleString, imageByteArray);
+			
+			//add the reply to the Question
+			question.addReply(reply);
+			
+			//check the result
+			assertTrue("Reply is not equal", replyString.equals(reply.getContent()));
+		}
+	     public void testGetTitle() {
+			//create a Question 
+			String username = "username";
+			String questionString = "A qustion";
+			String replyString = "A reply";
+			String titleString = "A title";
+			String imageByteArray = null;
+			Author author = new Author(username);
+			Reply reply = new Reply(replyString, author.getUsername());
+			Question question = new Question(questionString, author.getUsername(),
+					titleString, imageByteArray);
+			
+			
+			assertEquals(question.getTitle(), "A title");
+		}
+
+	    public void testGetID_question() {
+			//create a Question 
+			String username = "username";
+			String questionString = "A qustion";
+			String replyString = "A reply";
+			String titleString = "A title";
+			String imageByteArray = null;
+
+	            ArrayList<Reply> replyList = new ArrayList<Reply>();
+			ArrayList<Answer> answerList = new ArrayList<Answer>();
+			Map<String, Author> upvotedPerson = new HashMap<String, Author>();
+			long ID_question = new Date().getTime();
+			Long answerCount = (long) 0;
+			Long upvoteCount_question = (long) 0;
+
+	            Author author = new Author(username);
+	            Question question = new Question(questionString, author.getUsername(),
+					titleString, imageByteArray);
+	            assertEquals(question.getID(),ID_question);
+	     }          
+	     public void testSetID() {
+			//create a Question 
+			String username = "username";
+			String questionString = "A qustion";
+			String replyString = "A reply";
+			String titleString = "A title";
+			String imageByteArray = null;
+
+	            ArrayList<Reply> replyList = new ArrayList<Reply>();
+			ArrayList<Answer> answerList = new ArrayList<Answer>();
+			Map<String, Author> upvotedPerson = new HashMap<String, Author>();
+			Long ID_question = new Date().getTime();
+			Long answerCount = (long) 0;
+			Long upvoteCount_question = (long) 0;
+	            Long newID=(long) 100;
+
+	            Author author = new Author(username);
+	            Question question = new Question(questionString, author.getUsername(),
+					titleString, imageByteArray);
+	            question.setID(newID);
+	            assertSame(question.getID(),newID);
+	     }          
+	      public void testGetAnswerCount() {
+			//create a Question 
+			String username = "username";
+			String questionString = "A qustion";
+			String replyString = "A reply";
+			String titleString = "A title";
+			String imageByteArray = null;
+			String answerString= new String();
+
+	            ArrayList<Reply> replyList = new ArrayList<Reply>();
+			ArrayList<Answer> answerList = new ArrayList<Answer>();
+			Map<String, Author> upvotedPerson = new HashMap<String, Author>();
+			Long ID_question = new Date().getTime();
+			Long answerCount = (long) 0;
+			Long upvoteCount_question = (long) 0;
+
+	            Author author = new Author(username);
+	      Answer answer = new Answer(answerString, author.getUsername(),    imageByteArray);
+	           Question question = new Question(questionString, author.getUsername(),
+					titleString, imageByteArray);
+	            question.addAnswer(answer);
+	            assertEquals(question.getAnswerCount(),1);
+	     }          
+
+	       public void testGetAnswers() {
+			//create a Question 
+			String username = "username";
+			String questionString = "A qustion";
+			String replyString = "A reply";
+			String titleString = "A title";
+			String imageByteArray = null;
+			String answerString= new String();
+
+	            ArrayList<Reply> replyList = new ArrayList<Reply>();
+			ArrayList<Answer> answerList = new ArrayList<Answer>();
+			Map<String, Author> upvotedPerson = new HashMap<String, Author>();
+			Long ID_question = new Date().getTime();
+			Long answerCount = (long) 0;
+			Long upvoteCount_question = (long) 0;
+
+	            Author author = new Author(username);
+	      Answer answer = new Answer(answerString, author.getUsername(),    imageByteArray);
+	           Question question = new Question(questionString, author.getUsername(),
+					titleString, imageByteArray);
+	            question.addAnswer(answer);
+	            
+	            answerList.add(answer);
+	            
+	            assertEquals(question.getAnswers(),answerList);
+	     }
+	}
+
+
